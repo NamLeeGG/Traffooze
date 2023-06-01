bad5940a88b9af544132d7f5d6aa788a328a4639
NamLeeGG <motherclucker6969@gmail.com>
2023-06-02 00:10:30 +0800

    delete REST

--- TraffoozePrj/settings.py
+++ TraffoozePrj/settings.py
@@ -58,11 +58,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+"""
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ],
 }
+"""
+
 
 ROOT_URLCONF = 'TraffoozePrj.urls'
 
